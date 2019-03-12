dc333f170fca63602cd87089c2bc8742025de95f
yongtany <yongn1494@gmail.com>
2019-03-12 19:50:36 +0900

    Intergrate Prisma in our server

--- src/server.js
+++ src/server.js
@@ -9,4 +9,4 @@ const server = new GraphQLServer({ schema });
 server.express.use(logger('dev'));
 
 server.start({ port: PORT }, () =>
-console.log(`Server running on http://localhost:${PORT}`));
+console.log(`😁 Server running on http://localhost:${PORT}`));
