ecab533752843c1abcd62cd470910666f72d66c8
yongtany <yongn1494@gmail.com>
2019-03-14 14:45:37 +0900

    Send secret key email to login

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({path: path.resolve(__dirname, '.env')});
+
 import { adjectives, nouns } from './words';
+import nodemailer from 'nodemailer';
+import sgTransport from 'nodemailer-sendgrid-transport';
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives [randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+  const options = {
+    auth : {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD,
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: 'tanygram@tanyfrom.com',
+    to: address,
+    subject: '🔒Login Secret for Tanygram 🔒',
+    html: `Hello! Your login is secret is ${secret}. <br/>Copy paste on the app/website to log in.`
+  };
+  return sendMail(email);
 };
