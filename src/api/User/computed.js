6a109a39beb6022ef6e8e8fb563bb5ea47f82865
yongtany <yongn1494@gmail.com>
2019-03-17 12:06:00 +0900

    IsSelf and IsFollowing fields

---
+++ src/api/User/computed.js
@@ -0,0 +1,35 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        const exists = await prisma.$exists.user({
+          AND: [
+            {
+              id: parentId
+            },
+            {
+              followers_some: {
+                id: user.id
+              }
+            }
+          ]
+        });
+        return exists;
+      } catch {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  },
+};
