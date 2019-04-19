a345ad438879e4136ba1613f04ee666e45eae031
yongtany <yongn1494@gmail.com>
2019-04-19 18:46:12 +0900

    Refactor about existing username

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,10 +1,13 @@
 import { prisma } from '../../../../generated/prisma-client';
-import { FieldsOnCorrectTypeRule } from 'graphql';
 
 export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ username });
+      if(exists) {
+        throw Error("This username is already taken");
+      }
       await prisma.createUser({
         username,
         email,
